93a64e32981cecf7d050bfee831b9ad8216064e6
2434436656323 <debug3694@mail.ru>
2026-07-13 15:27:39 +0900

    остальные файлы (тестовые)

---
+++ test.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    putchar('H');
+    putchar('e');
+    putchar('l');
+    putchar('l');
+    putchar('o');
+    putchar(' ');
+    putchar('W');
+    putchar('o');
+    putchar('r');
+    putchar('l');
+    putchar('d');
+    putchar('!');
+    putchar(10);
+    putchar(13);
+    putchar('\n');
+    return 0;
+}
+
